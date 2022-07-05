eaade75007248f7543878231ed842ffd3522ab8f
MarinaKhorunzhina <101120283+MarinaKhorunzhina@users.noreply.github.com>
2022-07-05 21:32:28 +0300

    Request Authorization

--- Notification/AppDelegate.swift
+++ Notification/AppDelegate.swift
@@ -6,14 +6,17 @@
 //
 
 import UIKit
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
+    let notificationCenter = UNUserNotificationCenter.current()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        requestAutorisation()
+        
         return true
     }
 
@@ -30,7 +33,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
+// запрос ползователю на отправку уведомлений
+    func requestAutorisation() {
+        notificationCenter.requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            print("Permission graned: \(granted)")
+            
+            guard granted else { return }
+            self.getNotificationSettings()
+        }
+    }
+    // запрос настроек из центра уведомления приложения
+    func getNotificationSettings() {
+        notificationCenter.getNotificationSettings { (settings) in
+            print("Notification settings: \(settings)")
+        }
+    }
 
 }
 

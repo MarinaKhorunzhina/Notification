01e40a4a873ef400e904b6710975e8f9498383d6
MarinaKhorunzhina <101120283+MarinaKhorunzhina@users.noreply.github.com>
2022-07-13 19:28:26 +0300

    Push-notification FireBase

--- Notification/Notifications.swift
+++ Notification/Notifications.swift
@@ -7,10 +7,12 @@
 
 import UIKit
 import UserNotifications
+import Firebase
 
 class Notifications: NSObject, UNUserNotificationCenterDelegate {
     
     let notificationCenter = UNUserNotificationCenter.current()
+    let messagingDeleget = Messaging.messaging()
     
     func requestAutorization() {
         notificationCenter.requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
@@ -118,3 +120,10 @@ class Notifications: NSObject, UNUserNotificationCenterDelegate {
     }
 
 }
+extension Notifications: MessagingDelegate {
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("\nFirebase regisration token: \(String(describing: fcmToken))\n")
+    }
+}
+
